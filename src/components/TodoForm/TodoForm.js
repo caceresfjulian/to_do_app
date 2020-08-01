f67b548f5874231744f78c1ee38065d2dfba61c2
Julian Caceres <caceresf.julian@gmail.com>
2020-08-01 17:20:28 -0500

    Create folders and links

---
+++ src/components/TodoForm/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+class TodoForm extends React.Component {
+    render(){
+        return (
+            <img src={logo} className="App-logo" alt="logo" />            
+        )
+    }
+}
+
+export default TodoForm;
