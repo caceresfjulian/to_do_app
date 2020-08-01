a483e280fab0f6852309ae5d1f592b192ef7ba0b
Julian Caceres <caceresf.julian@gmail.com>
2020-08-01 17:59:06 -0500

    Set form layout

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -7,6 +7,14 @@ class TodoForm extends React.Component {
             <div className="container text-center">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <div className="card">
+                <h3 class="card-title pt-2">Task Generator</h3>
+                    <form>
+                        <div className="form-group">
+                            <label for="taskTitle">Title:</label>
+                            <input type="text" id="taskTitle" className="ml-2"/>
+                        </div>
+                        <button type="submit" className="btn btn-primary mb-2">Submit</button>
+                    </form>
                 </div>
             </div>
         )
