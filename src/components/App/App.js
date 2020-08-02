65dff93df71b04f8a139a01f69348f31b4834651
Julian Caceres <caceresf.julian@gmail.com>
2020-08-01 19:04:54 -0500

    Add test tasks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,12 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      todos: [],
+      todos: [
+        {title: 'Task 1', description: 'Description 1', priority:'high', asignee:'agent 1' },
+        {title: 'Task 2', description: 'Description 2', priority: 'low', asignee:'agent 2'},
+        {title: 'Task 3', description: 'Description 3', priority: 'normal', asignee:'agent 3'},
+        {title: 'Task 4', description: 'Description 4', priority: 'high', asignee: 'agent 4'}
+      ],
       value: '',
     }
     this.onChangeValue = this.onChangeValue.bind(this);
