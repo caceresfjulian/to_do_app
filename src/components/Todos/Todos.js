391eb60567af1d1da891eff0446055852e4bdeaf
Julian Caceres <caceresf.julian@gmail.com>
2020-08-01 22:44:06 -0500

    Change todo card layout

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Todos extends React.Component {
     render() {
         return (
-            <div className="card col-3 m-4">
+            <div className="card col-3 m-4 p-2">
                 <h5 className="card-title">{this.props.title}</h5>
                 <h5 className="card-title">{this.props.description}</h5>
                 <h5 className="card-title">{this.props.priority}</h5>
