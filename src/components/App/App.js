f2147ebbe739c5ffe7b416d843eb49d8da95c833
Julian Caceres <caceresf.julian@gmail.com>
2020-08-02 18:56:53 -0500

    Add navbar and adjust todos

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,63 +4,68 @@ import TodoForm from '../TodoForm/TodoForm';
 import TodoList from '../TodoList/TodoList';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       todos: [
-        {title: 'Task 1', description: 'Description 1', priority:'high', assignee:'agent 1' },
-        {title: 'Task 2', description: 'Description 2', priority: 'low', assignee:'agent 2'},
-        {title: 'Task 3', description: 'Description 3', priority: 'normal', assignee:'agent 3'},
-        {title: 'Task 4', description: 'Description 4', priority: 'high', assignee: 'agent 4'}
+        { title: 'Task 1', description: 'Description 1', priority: 'high', assignee: 'agent 1' },
+        { title: 'Task 2', description: 'Description 2', priority: 'low', assignee: 'agent 2' },
+        { title: 'Task 3', description: 'Description 3', priority: 'normal', assignee: 'agent 3' },
+        { title: 'Task 4', description: 'Description 4', priority: 'high', assignee: 'agent 4' }
       ],
       title: '',
       description: '',
-      priority:'high',
-      assignee:'agent 1',
+      priority: 'high',
+      assignee: 'agent 1',
     }
     this.onChangeValue = this.onChangeValue.bind(this);
     this.onAddItem = this.onAddItem.bind(this);
     this.onRemoveItem = this.onRemoveItem.bind(this);
   }
 
-  onChangeValue (event) {
-    let {name, value} = event.target;
-    this.setState({[name]: value })
-    console.log({name, value})
+  onChangeValue(event) {
+    let { name, value } = event.target;
+    this.setState({ [name]: value })
+    console.log({ name, value })
   }
 
-  onAddItem (event){
+  onAddItem(event) {
     event.preventDefault();
-    this.setState( state => {
-      const todos = [...state.todos, 
-        {title: state.title, 
-          description: state.description,
-          priority: state.priority,
-          assignee: state.assignee,
-        } ];
+    this.setState(state => {
+      const todos = [...state.todos,
+      {
+        title: state.title,
+        description: state.description,
+        priority: state.priority,
+        assignee: state.assignee,
+      }];
 
       return {
         todos
       }
-    })    
+    })
   }
 
-  onRemoveItem (index){
+  onRemoveItem(index) {
     this.setState({
-      todos: this.state.todos.filter( e => {
-      return index !== this.state.todos.indexOf(e) })
+      todos: this.state.todos.filter(e => {
+        return index !== this.state.todos.indexOf(e)
+      })
     })
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <TodoForm onChange={this.onChangeValue} onClick={this.onAddItem} todos={this.state.todos}/>
-          </div>
-          <div className="col-8">
-          <TodoList todos={this.state.todos} onClick={this.onRemoveItem}/>
+      <div>
+      <nav className="navbar navbar-light bg-light">Navbar</nav>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <TodoForm onChange={this.onChangeValue} onClick={this.onAddItem} todos={this.state.todos} />
+            </div>
+            <div className="col-8">
+              <TodoList todos={this.state.todos} onClick={this.onRemoveItem} />
+            </div>
           </div>
         </div>
       </div>
