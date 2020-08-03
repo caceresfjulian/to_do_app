967c69764f7bb85ff5cd5001f31ff928f053345a
Julian Caceres <caceresf.julian@gmail.com>
2020-08-02 19:15:26 -0500

    Adjust NavBar layout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-class NavBar extends React.Component{
-    render(){
-        return(
-            <nav className="navbar navbar-light bg-light">Tasks
-            <span class="badge badge-pill badge-danger">1</span>      
-          </nav>
+class NavBar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-light bg-light">
+                <div>
+                    <span>Tasks</span>
+                    <span className="badge badge-pill badge-danger mx-2">1</span>
+                </div>
+            </nav>
         )
     }
 }
