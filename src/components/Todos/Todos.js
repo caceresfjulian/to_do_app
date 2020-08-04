f2b6293e501936aa16fd4a3d34fb818c78d18e11
Julian Caceres <caceresf.julian@gmail.com>
2020-08-04 17:35:42 -0500

    Fix JS format

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
 class Todos extends React.Component {
-    applyColor(priority){
-        if (priority === 'high'){
-            return {backgroundColor:"#F7C5C2"}
-        } else if ( priority === 'normal'){
-            return {backgroundColor: "#B4F7A4"}
+    applyColor(priority) {
+        if (priority === 'high') {
+            return { backgroundColor: "#F7C5C2" }
+        } else if (priority === 'normal') {
+            return { backgroundColor: "#B4F7A4" }
         } else {
-            return {backgroundColor: "#A4B2F7"}
+            return { backgroundColor: "#A4B2F7" }
         }
     }
     render() {
         let title;
-        if (this.props.title.length > 11){
+        if (this.props.title.length > 11) {
             title = `${this.props.title.slice(0, 8)}...`
-        }else{
+        } else {
             title = this.props.title;
         }
         let description;
-        if (this.props.description.length > 13){
+        if (this.props.description.length > 13) {
             description = `${this.props.description.slice(0, 13)}...`
-        }else{
+        } else {
             description = this.props.description;
         }
         return (
@@ -28,7 +28,7 @@ class Todos extends React.Component {
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <h6 className="card-subtitle mb-2 text-muted">{this.props.assignee}</h6>
-                    <p className="card-text">{description}</p>                    
+                    <p className="card-text">{description}</p>
                     <h6 href="/" className="card-subtitle text-muted">{this.props.priority}</h6>
                     <button className="btn btn-danger mt-4 btn-sm" onClick={() => this.props.onClick(this.props.id)}>Delete <span className="badge badge-pill badge-light">{this.props.id + 1}</span></button>
                 </div>
