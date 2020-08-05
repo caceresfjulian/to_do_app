0beed0a980eda3c378c046134b038ddb872e6c3d
Julian Caceres <caceresf.julian@gmail.com>
2020-08-04 20:01:07 -0500

    Add agent filter layout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,11 +9,19 @@ class NavBar extends React.Component {
                     <span className="badge badge-pill badge-danger mx-2">{this.props.counter}</span>
                 </div>
                 <div>
-                <select className="form-control-sm border-dark" name="display" onChange={this.props.onChange}>
+                <select className="form-control-sm border-dark mx-4" name="display" onChange={this.props.onChange}>
                     <option value="all">All</option>
                     <option value="high">High</option>
                     <option value="normal">Normal</option>
                     <option value="low">Low</option>
+                </select>
+                <select className="form-control-sm border-dark" name="agentFilter">
+                    <option value="all">All agents</option>
+                    <option value="agent 1">Agent 1</option>
+                    <option value="agent 2">Agent 2</option>
+                    <option value="agent 3">Agent 3</option>
+                    <option value="agent 4">Agent 4</option>
+                    <option value="agent 5">Agent 5</option>
                 </select>
                 </div>
                 <span>{this.props.page/9} / {Math.ceil(this.props.counter/9)}</span>
