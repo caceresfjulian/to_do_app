f0ed81d20be13851c3294be196ad24a1e133094c
Julian Caceres <caceresf.julian@gmail.com>
2020-08-13 13:27:57 -0500

    Updates

---
+++ precache-manifest.c83694532f820539022c6559a4019bc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428ca1daeb5cf6299dca3c4f5afbf8ca",
+    "url": "/to_do_app/index.html"
+  },
+  {
+    "revision": "a1930e118143424888ab",
+    "url": "/to_do_app/static/css/main.f68e7645.chunk.css"
+  },
+  {
+    "revision": "e8c93adfe6a55dd22100",
+    "url": "/to_do_app/static/js/2.26a81f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do_app/static/js/2.26a81f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1930e118143424888ab",
+    "url": "/to_do_app/static/js/main.90de34ed.chunk.js"
+  },
+  {
+    "revision": "b9494022df6281f9cd34",
+    "url": "/to_do_app/static/js/runtime-main.1b8c55ac.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/to_do_app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
